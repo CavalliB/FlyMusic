0a1684e1bc708957f8deea0400a6db59ec13fcc0
CavalliB <Cerdopigman12@hotmail.com>
2025-10-16 17:09:22 -0300

    base

---
+++ src/canciones.js
@@ -0,0 +1,25 @@
+export const canciones = [
+  {
+    titulo: 'Yellow',
+    autor: 'Coldplay',
+    imagen: '/coldplay-band.jpg',
+    categoria: 'Alternative Rock',
+    duracion: '3:30'
+  },
+  {
+    titulo: 'Hey Jude',
+    autor: 'The Beatles',
+    imagen: '/the-beatles-band.jpg',
+    categoria: 'Rock',
+    duracion: '4:57'
+
+  },
+  {
+    titulo: 'asdf',
+    autor: 'asdfsdf',
+    imagen: '/the-beatles-band.jpg',
+    categoria: 'asdf',
+    duracion: '2:22'
+
+  },
+]
